2a01229f7602101eb7ee4c130423dfae9aaa30f8
petitlaye03 <124119995+petitlaye03@users.noreply.github.com>
2024-01-26 10:27:57 +0000

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,8 +1,6 @@
 # Importation des packages
 import pandas as pd
 import streamlit as st
-import seaborn as sns
-import matplotlib.pyplot as plt
 
 # Chargement des données depuis les fichiers CSV pour BeautifulSoup4
 villas_bs4 = pd.read_csv("Vente_Locations_Villas.csv")
@@ -56,54 +54,3 @@ elif choix == taches[1]:  # Si l'utilisateur choisit Web Scraper
         st.subheader("Voici les données des terrains extraites avec Web scraper :")
         st.write(terrains_ws)
         st.write("Nombre de lignes et de colonnes pour les données des terrains : ", terrains_ws.shape)
-else:
-        st.title("Données Web Scrapping")
-        st.write("Voici les differents graphiques  ")
-    # Simuler des données pour le tableau de bord
-    # Créer un DataFrame
-        df = pd.read_csv("Vente_Locations_Villas.csv")
-        da = pd.read_csv("Appartements.csv")
-        dt = pd.read_csv("Terrains.csv")
-    # Créer le diagramme camembert pour la variable "villa"
-        st.subheader("Taux Vente/Location ")
-        villa_counts = df['Type_annonce'].value_counts()
-    # Diagramme camembert directement dans la page
-        fig, ax = plt.subplots()
-        ax.pie(villa_counts.head(2), labels=villa_counts.head(2).index, autopct='%1.1f%%', startangle=90, colors=sns.color_palette('pastel'), wedgeprops=dict(width=0.4))
-        ax.axis('equal')
-        st.pyplot(fig)
-        st.subheader("Courbe de Régression")
-        fi, ax = plt.subplots()
-    # Utiliser l'axe pour créer un nuage de points
-        sns.regplot(data=da, x='Nombre_pieces', y='Prix', color='blue', scatter_kws={'s': 100})
-    # Personnaliser l'axe et le graphique
-        ax.set_xlabel('Nombre de Pièces')
-        ax.set_ylabel('Prix')
-        ax.set_title('Courbe de Régression Nombre de Pièces/Prix')
-    # Afficher le graphique avec Streamlit
-        st.pyplot(fi)
-        st.subheader("Courbe de Régression")
-    # Créer un objet de figure et un axe avec Matplotlib
-        fid, ax = plt.subplots()
-    # Utiliser l'axe pour créer un nuage de points
-        ax.scatter(dt['Superficie'], dt['Prix'], c='blue', label='Biens immobiliers')
-    # Personnaliser l'axe et le graphique
-        ax.set_xlabel('Superficie (m²)')
-        ax.set_ylabel('Prix (EUR)')
-        ax.set_title('Nuage de Points Prix/Superficie')
-    # Ajouter une légende
-        ax.legend()
-    # Afficher le graphique avec Streamlit
-        st.pyplot(fid)
-
-
-
-
-
-
-
-
-
-
-
-
